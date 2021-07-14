f0184e24439599f9d6aca564b289c03f5b3d81d1
Venkata krishnan Sowrirajan <vsowrirajan@linkedin.com>
2021-07-13 22:40:52 -0700

    fix RemotePushBlockResolver

--- core/src/main/scala/org/apache/spark/shuffle/IndexShuffleBlockResolver.scala
+++ core/src/main/scala/org/apache/spark/shuffle/IndexShuffleBlockResolver.scala
@@ -379,7 +379,7 @@ private[spark] class IndexShuffleBlockResolver(
    * knows how to consume local merged shuffle file as multiple chunks.
    */
   override def getMergedBlockData(
-      blockId: ShufflePushMergeBlockId,
+      blockId: ShufflePushBlockId,
       dirs: Option[Array[String]]): Seq[ManagedBuffer] = {
     val indexFile =
       getMergedBlockIndexFile(conf.getAppId, blockId.shuffleId, blockId.shuffleSequenceId,
@@ -408,7 +408,7 @@ private[spark] class IndexShuffleBlockResolver(
    * This is only used for reading local merged block meta data.
    */
   override def getMergedBlockMeta(
-      blockId: ShufflePushMergeBlockId,
+      blockId: ShufflePushBlockId,
       dirs: Option[Array[String]]): MergedBlockMeta = {
     val indexFile =
       getMergedBlockIndexFile(conf.getAppId, blockId.shuffleId,
